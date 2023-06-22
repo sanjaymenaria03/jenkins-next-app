62c53ecd64cbd6541f3f04bf963cd4c014fd59b1
= <=>
2023-06-22 10:00:08 +0530

    initial commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ["img.icons8.com", "aasthy.com", "images.unsplash.com","firebasestorage.googleapis.com"]
+    }
+}
+
+module.exports = nextConfig
